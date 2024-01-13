2f7b9cb25a7ea54d513dbde451a1d64204babb13
Muvinn <kbvi45@gmail.com>
2024-01-13 23:19:31 +0900

    24.01.13 솔브

---
+++ JYkim_BJ2/테스트1.py
@@ -0,0 +1,36 @@
+T = int(input())
+numList = []
+for t in range(T):
+
+    n = int(input())
+    if n == 0:
+        #del numList[-1]
+        numList.pop()
+    elif n != 0:
+        numList.append(n)
+    
+print(sum(numList))
+T = int(input())
+numList = []
+for t in range(T):
+
+    n = int(input())
+    if n == 0:
+        #del numList[-1]
+        numList.pop()
+    elif n != 0:
+        numList.append(n)
+    
+print(sum(numList))
+T = int(input())
+numList = []
+for t in range(T):
+
+    n = int(input())
+    if n == 0:
+        #del numList[-1]
+        numList.pop()
+    elif n != 0:
+        numList.append(n)
+    
+print(sum(numList))
